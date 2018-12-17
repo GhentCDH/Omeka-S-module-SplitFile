807f6a8d88060d6c32548ab7cb6266b43554a9dc
Jim Safley <jimsafley@gmail.com>
2018-12-17 10:46:11 -0500

    Move convert splits to abstract class

--- src/Splitter/AbstractSplitter.php
+++ src/Splitter/AbstractSplitter.php
@@ -46,4 +46,47 @@ abstract class AbstractSplitter implements SplitterInterface
         $pages = count(explode("\n", $output));
         return (int) $pages;
     }
+
+    /**
+     * Split a file using the convert command.
+     *
+     * Can't reliably split large files with one command due to ImageMagick
+     * resource limits on some systems ("cache resources exhausted" errors).
+     * Instead, execute the command in 10-page batches.
+     *
+     * Options:
+     *   - from_pdf: Set to true if the file to split is a PDF file
+     *
+     * @param string $filePath The path to the file
+     * @param srtring $targetDir The path of the dir to process files
+     * @param int $pageCount The page count of the file
+     * @param array $options
+     * @return array|false
+     */
+    public function splitUsingConvert($filePath, $targetDir, $pageCount, array $options = [])
+    {
+        $commandPath = $this->cli->getCommandPath('convert');
+        $uniqueId = uniqid();
+        $pagePattern = sprintf('%s/%s-%%d.jpg', $targetDir, $uniqueId);
+        $indexes = range(0, $pageCount - 1);
+        foreach (array_chunk($indexes, 10) as $indexChunk) {
+            $range = sprintf('%s-%s', reset($indexChunk), end($indexChunk));
+            $filePathWithRange = sprintf('%s[%s]', $filePath, $range);
+            $args = [$commandPath];
+            if (isset($options['from_pdf']) && $options['from_pdf']) {
+                $args[] = '-density 150';
+            }
+            $args[] = escapeshellarg($filePathWithRange);
+            $args[] = '-auto-orient';
+            $args[] = '-background white';
+            $args[] = '+repage';
+            $args[] = '-alpha remove';
+            $args[] = escapeshellarg($pagePattern);
+            $command = implode(' ', $args);
+            $this->cli->execute($command);
+        }
+        $filePaths = glob(sprintf('%s/%s-*.jpg', $targetDir, $uniqueId));
+        natsort($filePaths);
+        return $filePaths;
+    }
 }
--- src/Splitter/Pdf/Jpg.php
+++ src/Splitter/Pdf/Jpg.php
@@ -18,31 +18,7 @@ class Jpg extends AbstractSplitter
 
     public function split($filePath, $targetDir)
     {
-        $commandPath = $this->cli->getCommandPath('convert');
-        $uniqueId = uniqid();
-        $pagePattern = sprintf('%s/%s-%%d.jpg', $targetDir, $uniqueId);
-        // Can't reliably split large PDFs with one command due to ImageMagick
-        // resource limits on some systems ("cache resources exhausted" errors).
-        // Instead, execute the command in 10-page batches.
         $pageCount = $this->getPdfPageCount($filePath);
-        $indexes = range(0, $pageCount - 1);
-        foreach (array_chunk($indexes, 10) as $indexChunk) {
-            $range = sprintf('%s-%s', reset($indexChunk), end($indexChunk));
-            $filePathWithRange = sprintf('%s[%s]', $filePath, $range);
-            $commandArgs = [
-                $commandPath,
-                '-density 150',
-                escapeshellarg($filePathWithRange),
-                '-auto-orient',
-                '-background white',
-                '+repage',
-                '-alpha remove',
-                escapeshellarg($pagePattern),
-            ];
-            $this->cli->execute(implode(' ', $commandArgs));
-        }
-        $filePaths = glob(sprintf('%s/%s-*.jpg', $targetDir, $uniqueId));
-        natsort($filePaths);
-        return $filePaths;
+        return $this->splitUsingConvert($filePath, $targetDir, $pageCount, ['from_pdf' => true]);
     }
 }
